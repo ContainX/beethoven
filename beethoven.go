2f87142f177bf9ceea6c64303d1779de0ff3560b
Jeremy Unruh <junruh@C02Q41AMG8WM-M.local>
2016-09-17 20:17:43 -0700

    foundation - initial config package. loads local, remote and from env vars

---
+++ beethoven.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ContainX/beethoven/config"
+	"github.com/ContainX/depcon/pkg/logger"
+	"fmt"
+)
+
+const (
+	Usage = `
+Beethoven (Mesos/Marathon HTTP based Proxy)
+
+== Version: %s - Built: %s ==
+`
+	Example = `
+   Environment   : BT_MARATHON_URLS=http://host:8080,http://host2 beethoven serve
+   Local Config  : beethoven serve --config filepath
+   Remote Config : beethoven serve --remote http://confighost --name myapp --profile prod
+`
+)
+
+var (
+	// Added via ldflags
+	version = "-"
+	built = ""
+
+	// Root command is the parent to all other commands
+	rootCmd = &cobra.Command{
+		Use:   "beethoven [config-file | remote-server-url]",
+		Short: "Mesos/Marathon HTTP based Proxy",
+		Long: fmt.Sprintf(Usage, version, built),
+		Example: Example,
+	}
+
+	serveCmd = &cobra.Command{
+		Use: "serve",
+		Short: "Start serving traffic",
+		Run: serve,
+		Example: Example,
+	}
+
+	log = logger.GetLogger("beethoven")
+
+)
+
+func serve(cmd *cobra.Command, args []string) {
+	config, err := config.LoadConfigFromCommand(cmd)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(config)
+}
+
+func main() {
+	rootCmd.AddCommand(serveCmd)
+	config.AddFlags(serveCmd)
+	rootCmd.Execute()
+}
